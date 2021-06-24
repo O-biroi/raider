71d16fb309c1437e85b4be4244d2184a3a13f1dd
lizimai <zimai.li@evobio.eu>
2021-06-24 16:37:55 +0200

    Add movement as optional_vignette

--- inst/extdata/optional_vignette/animation.R
+++ inst/extdata/optional_vignette/animation.R
@@ -13,14 +13,28 @@ session <- data.frame(x = numeric(),
                       t = integer())
 x_init <- sample(xscale, 1, replace = TRUE)
 y_init <- sample(yscale, 1, replace = TRUE)
-session[1,] = c(x_init, y_init, 0)
+session[1,] = c(x_init, y_init, 1)
+direction_init <- runif(1, min = -2*pi, max = 2*pi )
+
+
+# create threshold level of movement
+max_speed <- 2
 
 
 # create a movement session
 for (i in 1:timescale){
+  # calculate next step direction
+    # if i = 1, initial direction
+    # if i > 1, current direction (calculated by the tangent of the current step
+    # and the step before) + a direction changer (normal distribution)
+  direction <- ifelse(i = 1, direction_init, rnorm(-pi, pi) + atan((session[i,]$y - session[i-1,]$y)/(session[i,]$x - session[i-1,]$x)))
+  x_change <- rnorm(-max_speed, max_speed)
+  y_change
+  c(sample(-1:1, 1), sample(-1:1, 1), 1)
+
   # judge whether ant is at the edge of the area
   # add new rows
-  session[nrow(session) + 1, ] <- session[nrow(session), ] + c(sample(-1:1, 1), sample(-1:1, 1), 1)
+  session[nrow(session) + 1, ] <- session[nrow(session), ]
   # if the new location exceed the scale of area, retreat one step back
   if (abs(session[nrow(session),]$x) - abs(max(xscale)) > 0){session[nrow(session),]$x <- session[nrow(session) -1 ,]$x}
   if (abs(session[nrow(session),]$y) - abs(max(yscale)) > 0){session[nrow(session),]$y <- session[nrow(session) -1 ,]$y}
